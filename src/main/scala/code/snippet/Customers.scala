1501198b41142827ada1c2bd4e0676c8b3aac229
Victor Perron <perron.victor@gmail.com>
2011-06-09 19:30:51 +0200

    Customers begin to be managed using ValueCells and RequestVars

--- src/main/scala/code/snippet/Customers.scala
+++ src/main/scala/code/snippet/Customers.scala
@@ -7,6 +7,7 @@ import common._
 import Helpers._
 import http._
 import dispatch.{Http, StatusCode}
+import js.JsCmd
 import js.JsCmds._
 import js.jquery._
 
@@ -16,39 +17,45 @@ import scala.xml.{NodeSeq, Text}
 import json._
 import record.field.{IntField, StringField}
 
-class Customers extends Logger{
+// Just a helper class to help me get the fields I need
+private object JObjectFieldHelper {
   import couchdb._
   import DocumentHelpers.jobjectToJObjectExtension
+  import couchdb.DocumentHelpers.JObjectExtension
 
-
-  // Just a helper class to help me get the fields I need
-  private object JObjectFieldHelper {
-    import couchdb.DocumentHelpers.JObjectExtension
-
-    implicit def toFieldHelper(obj: JObject): JObjectFieldHelper_Internal = new JObjectFieldHelper_Internal(obj)
-    class JObjectFieldHelper_Internal(obj: JObject) {
-      def field(name: String): String = obj.getString(name) open_!
-    }
+  implicit def toFieldHelper(obj: JObject): JObjectFieldHelper_Internal = new JObjectFieldHelper_Internal(obj)
+  class JObjectFieldHelper_Internal(obj: JObject) {
+    def field(name: String): String = obj.getString(name) open_!
   }
+}
 
 
+class Customers extends Logger{
+  import JObjectFieldHelper.toFieldHelper
 
 
   private object Data {
     import CustomerUtils._
 
-    def customers = {
-      val (http, db) = init
+    val (http, db) = init
 
-      val Full(record) = http(db design(db_designName) view("informations") query) 
+    def customers = {
+
+      val Full(record) = http((db design(db_designName) view("informations") limit(5) descending) query) 
 
       record.rows.flatMap(_.value.asA[JObject]).toList
     }
+
     
-    val total_customers = ValueCell(customers.length toString)
+    val total_customers = ValueCell( (Customer.all(identity).open_! length) toString )
   }
 
 
+  // ValueCell of all customers
+  val visibleCustomers = ValueCell[List[JObject]](Data.customers)
+
+  // RequestVar to retain selected user for the edition form
+  private object selectedCustomer extends RequestVar[JObject](new JObject(List()))
 
 
   def totalCustomers(in: NodeSeq) = WiringUI.asText(in, Data.total_customers, JqWiringSupport.slideDown)
@@ -56,22 +63,28 @@ class Customers extends Logger{
 
   def addCustomer(in: NodeSeq) = <span>[Button to add a user]</span>
 
+  def removeCustomer(c : JObject) : JsCmd  = {
+    S.notice("Customer "+c.field("FirstName")+" removed.")
+    Noop
+  }
 
   def showLines = {
-    import JObjectFieldHelper.toFieldHelper
+
+    // Don't reload the database every time
+    val customers = visibleCustomers.get
 
     S.notice(Data.total_customers.get + " customers were found in the CouchDB database.")
 
-    // Data.customers.foreach{ r: JObject => S.notice(Printer.pretty(render(r))) }
+    val alternate =  for(i <- List.range(0,customers.length))  yield if(i % 2 == 0) "tr0" else "tr1"
 
-    val alternate =  for(i <- List.range(0,Data.customers.length))  yield if(i % 2 == 0) "tr0" else "tr1"
-
-    "* *" #> (
-      for((c, _class) <- Data.customers zip alternate) 
-        yield  <tr class={_class + " span-12 last"}>
+    "#customer_lines *" #> (
+      for((c, _class) <- customers zip alternate) 
+        yield  <tr class={_class + " span-16 last"}>
           <td class="span-4">{c.field("FirstName")}</td>
           <td class="span-4">{c.field("LastName")}</td>
-          <td class="span-4"><i>{c.field("State")}</i></td></tr>
+          <td class="span-4"><i>{c.field("State")}</i></td>
+          <td class="span-2">{SHtml.link("/manage/edit", () => selectedCustomer(c), Text("Edit"))}</td>
+          <td class="span-2">{SHtml.a(Text("Delete"))(removeCustomer(c))}</td></tr>
     )
   }
 
